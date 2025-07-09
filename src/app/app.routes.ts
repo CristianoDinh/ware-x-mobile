35fdb5c40e975e91a00fbaf76177537cc9af7666
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-07-09 11:13:37 +0700

    finish route config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,25 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
   {
     path: 'folder/:id',
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./signup/signup.page').then( m => m.SignupPage)
+  },
+  {
+    path: 'recovery',
+    loadComponent: () => import('./recovery/recovery.page').then( m => m.RecoveryPage)
   },
 ];
